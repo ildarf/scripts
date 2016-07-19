9bb57d1395e918634cf54b0f510f4b8b826fb6b9
fatkullin <fatkullin@phil.hhu.de>
2016-07-19 09:25:26 +0200

    some added

---
+++ perltest.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+# get total arg passed to this script
+my $total = $#ARGV + 1;
+my $counter = 1;
+ 
+# get script name
+my $scriptname = $0;
+ 
+print "Total args passed to $scriptname : $total\n";
+ 
+# Use loop to print all args stored in an array called @ARGV
+foreach my $a(@ARGV) {
+	print "Arg # $counter : $a\n";
+	$counter++;
+}
+
+foreach (@ARGV) {
+	print "$_\n";
+}
